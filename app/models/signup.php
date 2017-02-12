45a2d4deabb5f0421c73d64717cc894d45cfabb2
Rounak Raghani <Rounak Raghani>
2017-02-12 15:35:14 +0530

    new file

---
+++ app/models/signup.php
@@ -0,0 +1,20 @@
+<?php
+
+class signup
+{
+	private $obj=NULL;
+	private $name=NULL;
+	private $phone=NULL;
+	private $pass=NULL;
+	private $email=NULL;
+	function __construct()
+	{
+		
+	}
+	function getvalues($obj)
+	{
+	   
+	}
+}
+
+?>
