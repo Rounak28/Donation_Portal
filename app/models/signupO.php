9fd96f980d61d1d49fe35e3706ce41c29ddb421d
Rounak28 <rounak.cba1547.ict.gnu.ac.in>
2017-02-12 20:48:44 +0530

    changes

---
+++ app/models/signupO.php
@@ -0,0 +1,28 @@
+<?php
+include '\db_connection.php';
+class signupO
+{
+private $orgbean=NULL;
+private $name=NULL;
+private $email=NULL;
+private $pass=NULL;
+private $phone=NULL;
+function __construct($orgbean)
+{
+	 $this->orgbean=$orgbean;
+	 $this->name=$orgbean->getName();
+	 $this->email=$orgbean->getEmail();
+	 $this->pass=$orgbean->getPhone();
+	 $this->phone=$orgbean->getPhone();
+	 
+}
+
+function store_org_data()
+{
+	echo $this->name;
+	$sql="INSERT INTO ORG VALUES('$this->name','$this->email',$this->phone,'$this->pass')";
+	$conn=new db_connection();
+	$conn->db_Conn($sql);
+}
+}
+?>
