9edfd37da60592c378e03e4010ce10bac48111e8
Rounak28 <rounak.cba1547.ict.gnu.ac.in>
2017-02-12 20:48:49 +0530

    changes

---
+++ app/models/signupU.php
@@ -0,0 +1,31 @@
+<?php
+include '\db_connection.php';
+class signupU
+{
+	private $usrbean;
+	private $name=NULL;
+	private $phone=NULL;
+	private $pass=NULL;
+	private $email=NULL;
+	function __construct($usrbean)
+	{
+		$this->usrbean=$usrbean;
+		$this->name=$usrbean->getName();
+		$this->email=$usrbean->getEmail();
+		$this->phone=$usrbean->getPhone();
+		$this->pass=$usrbean->getpass();
+		
+	}
+	
+	function store_usr_data()
+	{
+		
+		$sql="INSERT INTO USER VALUES('$this->name','$this->email',$this->phone,'$this->pass')";
+		$conn=new db_connection();
+		$conn->db_Conn($sql);
+		echo 'success';
+		
+	}
+}
+
+?>
