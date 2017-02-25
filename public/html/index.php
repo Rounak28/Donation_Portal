bc6a4e75932bbaf806e2c04a8e3f16a8c5a0e42b
Rounak28 <rounak.cba1547.ict.gnu.ac.in>
2017-02-26 02:58:44 +0530

    nanme changed just

---
+++ public/html/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{margin:0px;}
+header
+{
+width:1400px;
+height:250px;
+position:fixed;
+background-color:BLACK;
+box-shadow:2px 4px 4px 2px black;
+}
+.arrange{
+padding-top:120px;
+}
+a
+{color:BLUE;
+padding:10px;
+text-decoration:none;
+}
+
+</style>
+<meta charset="ISO-8859-1">
+<title>index</title>
+</head>
+<body>
+ <header>   
+ <div> 
+<a href="organization.html">Organization sign</a>
+<a href="user.html">User signup</a>
+<a href="login.html">Login</a>
+ </div> </header>
+</body>
+</html>
